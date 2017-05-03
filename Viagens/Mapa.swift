0ba237992a59646978003d2839f3e67137bd5224
Daniel <dsulmandealbuquerqu@luc.edu>
2017-05-02 21:12:45 -0300

    Autorização para usar o mapa funcionando

--- Viagens/Mapa.swift
+++ Viagens/Mapa.swift
@@ -13,6 +13,40 @@ import MapKit
 class Mapa: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
     
     @IBOutlet weak var map: MKMapView!
+    var gerenciadorLocalizacao = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configuraGerenciadorLocalizacao()
+    }
+    
+    func configuraGerenciadorLocalizacao(){
+        gerenciadorLocalizacao.delegate = self
+        gerenciadorLocalizacao.desiredAccuracy = kCLLocationAccuracyBest
+        gerenciadorLocalizacao.requestWhenInUseAuthorization()
+        gerenciadorLocalizacao.startUpdatingLocation()
+    }
+    
+    //Caso o usuário não autorize, mostrar o caminho da tela de configurações
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status != .authorizedWhenInUse{
+            let alertaController = UIAlertController(title: "Permissão de localização", message: "Necessário permissão para a sua localização", preferredStyle: .alert)
+            let acaoConfiguracao = UIAlertAction(title: "Abrir configuração", style: .default, handler: { (alertaConfiguracoes) in
+                if let configuracoes = NSURL(string: UIApplicationOpenSettingsURLString){
+                    UIApplication.shared.open(configuracoes as URL)
+                }
+            })
+            let acaoCancelar = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+            
+            alertaController.addAction(acaoConfiguracao)
+            alertaController.addAction(acaoCancelar)
+            
+            present(alertaController, animated: true, completion: nil)
+        }
+    }
+    
+    
     
     
 }
